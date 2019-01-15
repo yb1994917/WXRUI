f87c80bcb3770c0c24ff2d91211753748bf3bb84
Rattenking <1148063373@qq.com>
2019-01-15 16:59:04 +0800

    rui首页

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,17 +1,22 @@
-const bannerList = require('../../utils/banner.js');
+const list = require('../../utils/homeList.js');
 Page({
   onLoad(){
-    this.setData({ bannerList: bannerList})
+    this.setData({ homeList: list})
   },
   data: {
     current: 0,
     current_text: 0,
-    navList: ['选项1', '选项2', '选项3', '选项4', '选项5', '选项6', '选项7', '选项8', '选项9']
+    navList: ['选项1', '选项2', '选项3', '选项4', '选项5', '选项6', '选项7', '选项8', '选项9'],
+    html: '<view>这是一个view组件</view>',
+    nodes: `<div style="color:red;">Hello&nbsp;World!</div>`
   },
   changeFun(e){
     this.setData({ current: e.detail.current})
   },
   getIndex(e){
     this.setData({ current_text: e.detail.index})
+  },
+  tap(e){
+    console.log(e)
   }
 })
